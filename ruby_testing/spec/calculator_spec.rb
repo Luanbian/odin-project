01c6f6dd9960f36daee583468b7153f0f91328ac
luanbianchini <luanbianchine1@gmail.com>
2026-02-09 19:30:59 -0300

    feat: a bit of testing

---
+++ ruby_testing/spec/calculator_spec.rb
@@ -0,0 +1,10 @@
+require './calculator'
+
+describe Calculator do
+  describe '#add' do
+    it 'returns the sum of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(2, 3)).to eq(5)
+    end
+  end
+end
